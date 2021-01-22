ad1d5f97b65331894c94b5eb5a382134b05a7379
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2021-01-22 18:30:26 -0300

    feat: splash screen page

--- clone/mobile/deliver-ui/App.tsx
+++ clone/mobile/deliver-ui/App.tsx
@@ -1,33 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import logo from './assets/images/logo.png';
+import box from './assets/images/box.png';
+import background from './assets/images/background.png';
 
 import styles from './styles';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     'SFProDisplay-Bold': require('./assets/fonts/SFProDisplay-Bold.ttf'),
+    'SFProText-Regular': require('./assets/fonts/SFProText-Regular.ttf'),
+    'SFProText-Semibold': require('./assets/fonts/SFProText-Semibold.ttf'),
     Roboto_700Bold
   });
+
+  function handleOrderNow() {
+
+  }
+
+  function handleDismiss() {
+
+  }
 
   if (!fontsLoaded) {
     // return <AppLoading />;
     return <Text>Loading</Text>
   } else {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontFamily: 'SFProDisplay-Bold', fontSize: 40 }}>SFProDisplay-Bold</Text>
-        <Text style={{ fontFamily: 'Roboto_700Bold', fontSize: 40 }}>SFProDisplay-Bold</Text>
-      </View>
-      // <View>
-      //   <Image
-      //     source={logo}
-      //   />
+      // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      //   <Text style={{ fontFamily: 'SFProDisplay-Bold', fontSize: 40 }}>SFProDisplay-Bold</Text>
+      //   <Text style={{ fontFamily: 'Roboto_700Bold', fontSize: 40 }}>SFProDisplay-Bold</Text>
       // </View>
+      <View style={styles.container}>
+        <ImageBackground source={background} style={styles.background}>
+        <View style={styles.backdropTop}>
+          <Image
+            style={styles.logo}
+            source={logo}
+          />
+        </View>
+        <View style={styles.backdropBase}>
+          <Image
+              style={[styles.logo, styles.box]}
+              source={box}
+          />
+          <View style={styles.textContainer}>
+            <Text style={styles.title}>
+              Non-Contact Deliveries
+              </Text>
+            <Text style={styles.text}>
+              When placing an order, select the option “Contactless delivery” and
+              the courier will leave your order at the door.
+            </Text>
+          </View>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={styles.button} onPress={handleOrderNow}>
+              <Text style={[styles.orderButtonText, styles.buttonText]}>
+                ORDER NOW
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.dismissButton]} onPress={handleDismiss}>
+              <Text style={[styles.dismissButtonText, styles.buttonText]}>
+                DISMISS
+              </Text>
+            </TouchableOpacity>
+          </View>
+          </View>
+        </ImageBackground>
+      </View>
     );
   }
 }
+
--- clone/mobile/deliver-ui/styles.ts
+++ clone/mobile/deliver-ui/styles.ts
@@ -3,10 +3,86 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#A259FF",
   },
+
+  background: {
+    flex: 1,
+  },
+
+  backdropTop: {
+    marginTop: 63,
+    marginLeft: 20,
+    height: "30%",
+  },
+
+  backdropBase: {
+    flex: 1,
+    backgroundColor: "#F6F5F5",
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+
+    justifyContent: "center",
+  },
+
+  title: {
+    fontFamily: 'SFProDisplay-Bold',
+    fontSize: 34,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+
+  text: {
+    fontFamily: 'SFProText-Regular',
+    fontSize: 17,
+    textAlign: "center",
+    color: "#9586A8",
+    marginBottom: 20,
+  },
+
+  logo: {
+    width: 100,
+    height: 100,
+  },
+
+  box: {
+    marginTop: 20,
+    alignSelf: "center",
+  },
+
+  buttonsContainer: {
+    margin: 10,
+  },
+
+  button: {
+    marginBottom: 10,
+    alignItems: "center",
+    backgroundColor: "#0BCE83",
+    padding: 15,
+    borderRadius: 8,
+    height: 56,
+  },
+
+  dismissButton: {
+    backgroundColor: "#F6F5F5",
+  },
+
+  buttonText: {
+    fontFamily: 'SFProText-Semibold',
+  },
+
+  orderButtonText: {
+    color: "#FFFFFF",
+  },
+
+  dismissButtonText: {
+    color: "#9586A8",
+  },
+
+  textContainer: {
+    margin: 20,
+  }
 });
 
 export default styles;
+
